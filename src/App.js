e220cbad7c7791f5e44030e5347a24d64a3bbd9e
ahmedmalick <ahmedmalik2k20@gmail.com>
2021-01-04 22:29:56 +0500

    finally come up with red queen race app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
+import useWebAnimations from "@wellyshen/use-web-animations";
+import cloud from  './images/cloud.png';
+import cycle from  './images/cycle.gif';
 import './App.css';
 
 function App() {
+
+  const { ref, getAnimation: cycleAnimate } = useWebAnimations({
+    keyframes: {
+      transform: "translateX(160)",
+      transform: "translateX(320px)",
+    },
+    timing: {
+      duration: 2000,
+      iterations: Infinity,
+      direction: "alternate",
+      easing: "ease-in-out",
+    },
+  });
+
+  const { ref: cloud1 } = useWebAnimations({
+    keyframes: {
+      transform: "translateX(1500px)",
+      transform: "translateX(-1500px)",
+    },
+    timing: {
+      duration: 1500,
+      iterations: Infinity,
+      direction: "alternate",
+      easing: "ease-in-out",
+    },
+  });
+
+  const { ref: cloud2 } = useWebAnimations({
+    keyframes: {
+      transform: "translateX(1500px)",
+      transform: "translateX(-1500px)",
+    },
+    timing: {
+      duration: 1500,
+      iterations: Infinity,
+      direction: "alternate",
+      easing: "ease-in-out",
+    },
+  });
+
+  const speedUp = () => {
+   const cycleSpeed = cycleAnimate();
+   cycleSpeed.updatePlaybackRate(cycleSpeed.playbackRate * 1.1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img className="img-1" src={cloud} alt="cloud" ref={cloud1} />
+      <img className="img-2" src={cloud} alt="cloud" ref={cloud2} />
+      <img className="cycle" src={cycle} alt="cycle" onClick={speedUp} ref={ref} />
     </div>
   );
 }
