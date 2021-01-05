188ddf46619d31034cdfff1ffad1a69a4d58b6dc
ahmedmalick <ahmedmalik2k20@gmail.com>
2021-01-05 21:39:10 +0500

    updated last time

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import useWebAnimations from "@wellyshen/use-web-animations";
+import useWebAnimations, { pulse } from "@wellyshen/use-web-animations";
 import cloud from  './images/cloud.png';
 import cycle from  './images/cycle.gif';
 import './App.css';
 
 function App() {
 
-  const { ref, getAnimation: cycleAnimate } = useWebAnimations({
+  const { ref: cycle1, getAnimation: cycleAnimate } = useWebAnimations({
     keyframes: [
-      {transform: "translateX(160)"},
-      {transform: "translateX(320px)"},
+      {transform: "translateX(0)"},
+      {transform: "translateX(700px)"},
     ],
     timing: {
       duration: 2000,
@@ -19,42 +19,46 @@ function App() {
     },
   });
 
-  const { ref: cloud1 } = useWebAnimations({
+  const { ref: cloud1, getAnimation: cloud1Animate } = useWebAnimations({
     keyframes: [
       {transform: "translateX(1500px)"},
       {transform: "translateX(-1500px)"},
     ],
     timing: {
-      duration: 1500,
+      duration: 4000,
       iterations: Infinity,
-      direction: "alternate",
-      easing: "ease-in-out",
+      // direction: "alternate",
+      // easing: "ease-in-out",
     },
   });
 
-  const { ref: cloud2 } = useWebAnimations({
+  const { ref: cloud2, getAnimation: cloud2Animate } = useWebAnimations({
     keyframes: [
       {transform: "translateX(1500px)"},
       {transform: "translateX(-1500px)"},
     ],
     timing: {
-      duration: 1500,
+      duration: 4000,
       iterations: Infinity,
-      direction: "alternate",
-      easing: "ease-in-out",
     },
   });
+
+  const { keyframes, timing } = pulse;
 
   const speedUp = () => {
    const cycleSpeed = cycleAnimate();
+   const cloud1Speed = cloud1Animate();
+   const cloud2Speed = cloud2Animate();
    cycleSpeed.updatePlaybackRate(cycleSpeed.playbackRate * 1.1);
+   cloud1Speed.updatePlaybackRate(cloud1Speed.playbackRate * 1.1);
+   cloud2Speed.updatePlaybackRate(cloud2Speed.playbackRate * 1.1);
   };
 
   return (
     <div>
       <img className="img-1" src={cloud} alt="cloud" ref={cloud1} />
       <img className="img-2" src={cloud} alt="cloud" ref={cloud2} />
-      <img className="cycle" src={cycle} alt="cycle" onClick={speedUp} ref={ref} />
+      <img className="cycle" src={cycle} alt="cycle" onClick={speedUp} ref={cycle1} />
     </div>
   );
 }
